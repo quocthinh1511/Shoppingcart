47bc1a34a337bcdbada391874cdd3f061a1e0115
Quoc Thinh <lquocthinh10a1@gmail.com>
2022-09-27 15:15:03 +0700

    test_heroku

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,12 +1,8 @@
 class CartController < ApplicationController
 
   def show 
-    @order = current_order
     @order_items = current_order.order_items
     #@order.update(order_params)
     #current_order.shipping_id = @order.shipping_id
   end
-
-  private
-
 end
